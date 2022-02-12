af31050846e436db2ba20552984204dc17f1d94c
mis <1446742876@qq.com>
2022-02-12 23:20:42 +0800

    测试字符串

--- day01/test-string.py
+++ day01/test-string.py
@@ -1,8 +1,39 @@
-str = 'Hello World!'
 
-s = {a for a in str if a not in 'elo'}
+# str = 'Hello World!'
+#
+# s = {a for a in str if a not in 'elo'}
+#
+# print(s)
+#
+# i = {k for k in range(0,len(str))}
+# print(i)
 
-print(s)
+# print("\a")
+#
+#
+# print('hello \v world')
+#
+# print("Hello\rWorld!")
+# print('google runoob taobao\r1234')
 
-i = {k for k in range(0,len(str))}
-print(i)
+a = "Hello"
+b = "Python"
+
+print("a + b 输出结果：", a + b)
+print("a * 2 输出结果：", a * 2)
+print("a[1] 输出结果：", a[1])
+print("a[1:4] 输出结果：", a[1:4])
+
+if ("H" in a):
+    print("H 在变量 a 中")
+else:
+    print("H 不在变量 a 中")
+
+if ("M" not in a):
+    print("M 不在变量 a 中")
+else:
+    print("M 在变量 a 中")
+
+
+
+print ("我叫 %s 今年 %d 岁!" % ('小明', 10))
