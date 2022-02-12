0ef61c12eb50b3929850cf3a3b90a5b9b1a9ebeb
mis <1446742876@qq.com>
2022-02-12 22:50:18 +0800

    推导式

--- day01/test-tuidaoshi.py
+++ day01/test-tuidaoshi.py
@@ -1,5 +1,5 @@
 
-#列表推导式
+#列表推导式(list)
 #names = ['Bob','Tom','alice','Jerry','Wendy','Smith']
 #
 # new_name = [name.upper()for name in names if len(name)>3]
@@ -9,7 +9,7 @@
 #
 # print(multiples)
 
-#字典推导式
+#字典推导式(dict)
 # listdemo = ['Google','Runoob', 'Taobao','Bob','Tom','alice','Jerry','Wendy','Smith']
 #
 # newdict = {key:len(key) for key in listdemo}
@@ -17,7 +17,19 @@
 # print(newdict)
 #
 # dic = {x:x**2 for x in (2,4,6)}
-# print(dic)
+# print(type(dic))
 
-#集合推导式
+#集合推导式(set)
+# setnew = {i**2 for i in (1,2,3)}
+# print(setnew)
+#
+# a = {x for x in 'abracadabraqwesdf' if x not in 'abc'}
+# print(a)
 
+#元组推导式(tuple)
+
+a = {x for x in range(1,10)}
+print(a)
+print(type(a))
+b = tuple(a)
+print(type(b))
