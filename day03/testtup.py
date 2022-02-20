37270d9bc66da76aa8ae6dd919ebb051efaf534b
mis <1446742876@qq.com>
2022-02-20 09:35:52 +0800

    测试tup&while

---
+++ day03/testtup.py
@@ -0,0 +1,17 @@
+tup1 = ('Google','Runoob',1997,2000)
+tup2 = (1,2,3,4,5)
+tup3 = "a","b","c","d"
+print(tup1)
+print(tup2)
+print(tup3)
+print(type(tup3))
+
+i = 1
+sum = 0
+
+while i<101:
+    if i % 2 ==1:
+        sum = i + sum
+    i = i + 1
+
+print(sum)
